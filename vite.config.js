d4d1872eaccaed24f113168644a7109490fceb94
FishMoies <xisiyao0529@gmail.com>
2026-04-22 21:38:58 +0800

    Update vite.config.js
    更新筛选方法

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/RedWeb/',
-  plugins: [vue()],
+export default defineConfig(() => {
+  const base = process.env.VITE_BASE || '/'
+
+  return {
+    base,
+    plugins: [vue()],
+  }
 })
